6d33213e97ed30c9c6c67b192ef6cd5857689fe2
xiaohesong <zhushisong@youzan.com>
2020-05-04 19:27:14 +0800

    add fix

--- src/App.js
+++ src/App.js
@@ -13,10 +13,9 @@ const Components = [
   {value: 'react.(suspense&lazy)', id: 'Suspense'}
 ]
 
-
-
 function App() {
   const [renderer, setRenderer] = React.useState('react.element')
+  const [valueT, setValueT] = React.useState('{"$$typeof":"0xeac7","key":null,"type":"span","props":{"children":"Hi"},"ref":null}')
   const selected = Components.find(item => item.value === renderer)
   const Component = React.lazy(() => import(`./element-types/${selected.id}`))
   const options = Components.map((item, i) => {
@@ -27,6 +26,12 @@ function App() {
 
   const handleChange = e => setRenderer(e.target.value)
 
+  const handleValueChannge = e => {
+    setValueT(e.target.value)
+  }
+
+  const tValue = JSON.parse(valueT)
+  tValue.$$typeof = Symbol.for('react.element')
   return (
     <div className="App" id='app'>
       <select value={renderer} onChange={handleChange}>
@@ -35,6 +40,10 @@ function App() {
       <React.Suspense fallback={<div>loading...</div>}>
         <Component />
       </React.Suspense>
+      {/* <p></p>
+      <textarea value={valueT} style={{width: 240, height: 60}} onChange={handleValueChannge}/>
+      <p></p>
+      {tValue} */}
     </div>
   );
 }
